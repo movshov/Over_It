c252d042306d97e54e0c5ef2e3749da539e81ffa
movshov <movshov@pdx.edu>
2020-10-06 03:19:36 +0000

    Finished Assignment 1 Step E. Also added .gitignore for __pycache__ folder.

--- sreproject/settings.py
+++ sreproject/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'sreproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'sreproject',
+        'USER': 'sreprojectuser',
+        'PASSWORD': '<sudo>',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
@@ -118,3 +122,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+import os
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
